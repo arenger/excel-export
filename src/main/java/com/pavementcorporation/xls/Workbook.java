cdd0a8b84a918bd2ded379706b1db03969fc913b
A Renger <arenger@gmail.com>
2017-07-14 15:52:43 -0400

    clients loaded

--- src/main/java/com/pavementcorporation/xls/Workbook.java
+++ src/main/java/com/pavementcorporation/xls/Workbook.java
@@ -1,10 +1,12 @@
 package com.pavementcorporation.xls;
 
 import com.pavementcorporation.xls.dto.Client;
-import org.apache.poi.hssf.usermodel.HSSFSheet;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -18,20 +20,42 @@ public class Workbook {
 
    public Workbook(File xlsFile) throws IOException {
       try (FileInputStream in = new FileInputStream(xlsFile)) {
-         HSSFWorkbook wb = new HSSFWorkbook(in);
-         HSSFSheet sheet = wb.getSheet("Clients");
-         Iterator<Row> i = sheet.rowIterator();
-         while (i.hasNext()) {
-            Row r = i.next();
-            Client c = new Client((int)r.getCell(0).getNumericCellValue());
-            c.setName(r.getCell(1).getStringCellValue());
-            c.setAddress(r.getCell(2).getStringCellValue());
-            c.setCity(r.getCell(3).getStringCellValue());
-            c.setState(r.getCell(4).getStringCellValue());
-            c.setZip(r.getCell(5).getStringCellValue());
-            clients.put(c.getId(), c);
-            System.out.println(c);
+         XSSFWorkbook wb = new XSSFWorkbook(in);
+         loadClients(wb.getSheet("Clients"));
+      }
+   }
+
+   private String cellStringNvl(Cell c) {
+      if (c == null) {
+         return null;
+      }
+      if (c.getCellTypeEnum() == CellType.NUMERIC) {
+         return String.valueOf((int)c.getNumericCellValue());
+      } else {
+         return c.getStringCellValue();
+      }
+   }
+
+   private void loadClients(XSSFSheet sheet) {
+      Iterator<Row> i = sheet.rowIterator();
+      boolean firstRow = true;
+      while (i.hasNext()) {
+         Row r = i.next();
+         if (firstRow) {
+            firstRow = false;
+            continue;
          }
+         if ((r.getCell(0) == null) || (r.getCell(1) == null)) {
+            continue;
+         }
+         Client c = new Client((int)r.getCell(0).getNumericCellValue());
+         c.setName(cellStringNvl(r.getCell(1)));
+         c.setAddress(cellStringNvl(r.getCell(2)));
+         c.setCity(cellStringNvl(r.getCell(3)));
+         c.setState(cellStringNvl(r.getCell(4)));
+         c.setZip(cellStringNvl(r.getCell(5)));
+         clients.put(c.getId(), c);
+         System.out.println(c);
       }
    }
 }
