1845b13ad88e48e185fa522b3cf7c7cb29050cfe
A Renger <arenger@gmail.com>
2017-07-14 16:59:12 -0400

    appears that poi does not yet support array forumla evaluation

---
+++ src/main/java/com/pavementcorporation/xls/dto/Project.java
@@ -0,0 +1,78 @@
+package com.pavementcorporation.xls.dto;
+
+public class Project extends IdObj {
+   private final int clientId;
+
+   private String  name;
+   private String  amount;
+   private Boolean invoiced;
+   private String  manager;
+   private String  accountManager;
+   private String  startDate;
+   private String  endDate;
+
+   public Project(int id, int clientId) {
+      super(id);
+      this.clientId = clientId;
+   }
+
+   public int getClientId() {
+      return clientId;
+   }
+
+   public String getName() {
+      return name;
+   }
+
+   public void setName(String name) {
+      this.name = name;
+   }
+
+   public String getAmount() {
+      return amount;
+   }
+
+   public void setAmount(String amount) {
+      this.amount = amount;
+   }
+
+   public Boolean getInvoiced() {
+      return invoiced;
+   }
+
+   public void setInvoiced(Boolean invoiced) {
+      this.invoiced = invoiced;
+   }
+
+   public String getManager() {
+      return manager;
+   }
+
+   public void setManager(String manager) {
+      this.manager = manager;
+   }
+
+   public String getAccountManager() {
+      return accountManager;
+   }
+
+   public void setAccountManager(String accountManager) {
+      this.accountManager = accountManager;
+   }
+
+   public String getStartDate() {
+      return startDate;
+   }
+
+   public void setStartDate(String startDate) {
+      this.startDate = startDate;
+   }
+
+   public String getEndDate() {
+      return endDate;
+   }
+
+   public void setEndDate(String endDate) {
+      this.endDate = endDate;
+   }
+}
